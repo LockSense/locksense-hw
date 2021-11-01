4298b4fd3561f2aeadee259765681c03e03d5b48
Dorcas Tan <46101221+dorcastan@users.noreply.github.com>
2021-11-02 00:48:17 +0800

    Update single-sample recording duration

--- pcmreader.py
+++ pcmreader.py
@@ -9,6 +9,15 @@ def get_script_path():
  return os.path.dirname(os.realpath(sys.argv[0]))
 
 
+device_name = "COM3"
+
+baudrate = 256000
+bytes_per_read = 384
+num_seconds = 32
+
+bytes_per_second = baudrate / 8
+num_frames = int(bytes_per_second * num_seconds / bytes_per_read)
+
 def save_file(data):
  filename = time.strftime("pdm_test_%Y-%m-%d_%H-%M-%S_pcm")
  print("................saving file........")
@@ -22,25 +31,26 @@ def save_file(data):
 
 try:
  ser = None
- ser = Serial("COM3",256000,timeout=0.1)
+ ser = Serial(device_name,baudrate,timeout=0.1)
  pcmdata = bytearray()
   
  frame = 0
  while True:
-  indata = ser.read(384)
+  indata = ser.read(bytes_per_read)
   pcmdata+=bytearray(indata)
     
   #print(type(pcmdata))
   #pcmdata.append(arr) 
 
   #collect 1000 frames
-  if frame == 200 :
+  if frame == num_frames:
    #pcmdata.reverse()
    save_file(pcmdata)
-   frame=0
-  else:
-   frame+=1
+   break
+  elif frame % 10 == 0:
    print(frame)
+
+  frame+=1
 
      
 except SerialException as e:
