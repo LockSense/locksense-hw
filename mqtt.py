2f1ef90897d8a98e050a4d8857e17be849ab7995
steve <e0407222@u.nus.edu>
2021-11-18 15:24:08 +0800

    update mqtt names

--- mqtt.py
+++ mqtt.py
@@ -4,50 +4,32 @@ import wave
 import json
 import tensorflow as tf
 
+audio_src = 0
+
 def save_wav(decoded):
+    global audio_src
     bb = bytearray(decoded)
     filename = time.strftime("pdm_test_%Y-%m-%d_%H-%M-%S_adpcm")
-    w = wave.open("mqtt_samples/"+filename + ".wav", "w")
+    if audio_src =0:
+        w = wave.open("AudioRepo/cat1/"+filename + ".wav", "w")   
+    elif audio_src =1 
+        w = wave.open("AudioRepo/cat2/"+filename + ".wav", "w")
     w.setnchannels(1)
     w.setframerate(16000)
     w.setsampwidth(2)
     w.writeframes(bb)
     w.close()
 
-
-session = tf.compat.v1.Session(graph = tf.compat.v1.Graph())
-#initialize = tf.compat.v1.global_variables_initializer()
-model = None
-classes = ["daisy","dandelion","roses","sunflowers","tulips"]
-
-
-
-def model_loader():
-    global model
-    if model == None: 
-        model = load_model(MODEL_NAME)
-    return model
-
-
-
-def classify_audio(filename,image):
-    with session.graph.as_default():
-         print("*******start classifying")
-         set_session(session)
-         print("*******loading model")
-         model = model_loader()
-         print("*******model loaded")
-         result = model.predict(image)
-         themax = np.argmax(result)   
-    #return {predType:audPred/imgPred, fileName:fileName, prediction:0/1/2}
-
 def on_connect(client,userdata,flags,rc):
      print("Connected with result code " + str(rc))
      client.subscribe("Audio/Sensor1/Start")
      client.subscribe("Audio/Sensor1/Data")
+     client.subscribe("Audio/Sensor2/Start")
+     client.subscribe("Audio/Sensor2/Data")
      client.subscribe("Manager/#")
 
 def on_message(client,userdata,msg):
+    global audio_src
     if "Start" in msg.topic:
         print(msg.payload.decode("utf-8"))
     elif "Data" in msg.topic:
@@ -57,9 +39,16 @@ def on_message(client,userdata,msg):
          result=classify_audio(recv_dict["filename"])
          print("Sending Results: ",result)
          client.publish("Manager/category1/topic4",json.dumps(result))
-        else:
+        elif "Sensor1" in msg.topic:
          #Save Audio file
          print("Saving Audio File...")
+         audio_src =0
+         save_wav(msg.payload)
+         print("Done Saving!")
+        elif "Sensor2" in msg.topic:
+         #Save Audio file
+         print("Saving Audio File...")
+         audio_src =1
          save_wav(msg.payload)
          print("Done Saving!")
 
